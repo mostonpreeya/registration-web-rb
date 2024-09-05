95786a3bc21d49cc6f9058ecd3359cd5d7cfdf79
mostonpreeya <mostonpreeya@odds.team>
2024-09-05 10:01:32 +0700

    Implement controller for saving data to database

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,18 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :birthday, :gender, :email, :phone, :subject)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  get 'user/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'user/new', to: 'user#new'
+  post 'user', to: 'user#create'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'user#new'
 end
